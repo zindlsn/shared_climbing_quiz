71756d248b929fae1f0e23eab3215f4842a93808
sz <stefan.zindl@outlook.de>
2023-01-30 19:46:26 +0100

    Update Rating Question

--- lib/src/models/question_rating.dart
+++ lib/src/models/question_rating.dart
@@ -1,12 +1,31 @@
-
+import 'package:enum_to_string/enum_to_string.dart';
 import 'package:shared_climbing_quiz/src/models/level.dart';
 
 class QuestionRating {
   String questionId;
-  Level rateLevel;
+  Level? rateLevel;
   bool likedQuestion;
   String comment;
 
   QuestionRating(
-      this.questionId, this.rateLevel, this.likedQuestion, this.comment);
+      {required this.questionId,
+      this.rateLevel,
+      required this.likedQuestion,
+      required this.comment});
+
+  Map<String, dynamic> toJson() => {
+        'questionId': questionId,
+        'rateLevel': EnumToString.convertToString(rateLevel, camelCase: true),
+        'likedQuestion': likedQuestion,
+        'comment': comment,
+      };
+
+  static fromJson(Map<String, dynamic> parsedJson) {
+    return QuestionRating(
+        questionId: parsedJson['questionId'],
+        rateLevel:
+            EnumToString.fromString(Level.values, parsedJson['rateLevel'])!,
+        likedQuestion: parsedJson['likedQuestion'],
+        comment: parsedJson['comment']);
+  }
 }
--- test/shared_climbing_quiz_test.dart
+++ test/shared_climbing_quiz_test.dart
@@ -2,15 +2,5 @@ import 'package:shared_climbing_quiz/shared_climbing_quiz.dart';
 import 'package:test/test.dart';
 
 void main() {
-  group('A group of tests', () {
-    final awesome = Awesome();
 
-    setUp(() {
-      // Additional setup goes here.
-    });
-
-    test('First Test', () {
-      expect(awesome.isAwesome, isTrue);
-    });
-  });
 }
