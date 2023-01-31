d4848d9f40d13cce39fac7d59b08394fd7faa902
sz <stefan.zindl@outlook.de>
2023-01-31 19:41:41 +0100

    Adding createdAt Time

--- lib/src/models/question_rating.dart
+++ lib/src/models/question_rating.dart
@@ -1,29 +1,32 @@
-import 'package:enum_to_string/enum_to_string.dart';
-
 class QuestionRating {
   String questionId;
   int difficultRating = 0;
   bool? likedQuestion;
   String comment;
+  late DateTime createdAt;
 
   QuestionRating(
       {required this.questionId,
       required this.difficultRating,
       required this.likedQuestion,
-      required this.comment});
+      required this.comment}) {
+    createdAt = DateTime.now();
+  }
 
   Map<String, dynamic> toJson() => {
         'questionId': questionId,
         'difficultRating': difficultRating,
         'likedQuestion': likedQuestion,
         'comment': comment,
+        'createdAt': createdAt.toString()
       };
 
   static fromJson(Map<String, dynamic> parsedJson) {
     return QuestionRating(
-        questionId: parsedJson['questionId'],
-        difficultRating: parsedJson['difficultRating'],
-        likedQuestion: parsedJson['likedQuestion'],
-        comment: parsedJson['comment']);
+      questionId: parsedJson['questionId'],
+      difficultRating: parsedJson['difficultRating'] as int,
+      likedQuestion: parsedJson['likedQuestion'] as bool,
+      comment: parsedJson['comment'],
+    )..createdAt = DateTime.parse(parsedJson['createdAt']);
   }
 }
