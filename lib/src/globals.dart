e14dcf07f424efbfbdf2fa119ea7c6bcef1a1027
sz <stefan.zindl@outlook.de>
2023-03-27 20:09:18 +0200

    Update
    (cherry picked from commit c5aed23c912d028e332b231c9122bb3bdc0e9be4)

---
+++ lib/src/globals.dart
@@ -0,0 +1,4 @@
+import 'package:flutter/foundation.dart';
+
+String kQuestionTable = kDebugMode ? "questions2" : "questions2";
+String kLocalStorageQuestionId = kDebugMode ? 'testQuestionsAsJson' : 'questionsAsJson';
--- lib/src/services/question_service.dart
+++ lib/src/services/question_service.dart
@@ -1,80 +1,57 @@
+import 'dart:convert';
+
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/foundation.dart';
 import 'package:shared_climbing_quiz/shared_climbing_quiz.dart';
+import 'package:shared_climbing_quiz/src/globals.dart';
 import 'package:shared_climbing_quiz/src/services/storage_service.dart';
 
 class QuestionService {
   FirebaseFirestore? db = FirebaseFirestore.instance;
   LocalStorageService storageService = LocalStorageService();
 
-  int? lastQuestionNumber;
-
   Future addSimpleQuestion(SelectQuestion question) async {
-    DocumentReference document =
-    await db!.collection("questions").add(question.toJson());
-    await db!
-        .collection('questions')
-        .doc(document.id)
-        .collection('answers')
-        .add(question.answer.toJson());
+    DocumentReference document = await db!.collection(kQuestionTable).add(
+        question.toJson());
+    await db!.collection(kQuestionTable).doc(document.id).update(
+        {"answers": question.answer.toJson()});
   }
 
-  Future<void> deleteQuestion(String id) async {
-    await db!.collection('questions').doc(id).delete();
+  Future deleteQuestion(String id) async {
+    await db!.collection(kQuestionTable).doc(id).delete();
   }
 
-  Future saveLastQuestionNumber(int id) async {
-    bool questionNumberSavedSuccessfully =
-    await storageService.saveLastQuestionNumber(id);
-    print(questionNumberSavedSuccessfully);
-  }
-
-  Future<void> loadLastQuestionNumber() async {
-    lastQuestionNumber = await storageService.loadLastQuestionNumber();
-  }
-
-  Future<List<SelectQuestion>> getAllQuestionsAsync(
-      {bool onlyActive = false}) async {
+  Future<List<SelectQuestion>> getAllQuestionsAsync() async {
     List<SelectQuestion> result = [];
-    if (!await storageService.hasQuestionsSavedAsync()) {
+    if (true) {
       try {
         final db = FirebaseFirestore.instance;
-        Query<Map<String, dynamic>> questionsDocs = await db
-            .collection('questions')
-            .orderBy('createdTime', descending: true);
-
-        if (onlyActive) {
-          questionsDocs.where('isActive', isEqualTo: onlyActive);
-        }
-
-        QuerySnapshot<Map<String, dynamic>> docs = await questionsDocs.get();
+        QuerySnapshot<Map<String, dynamic>> questionsDocs = await db.collection(
+            kQuestionTable).orderBy('createdTime', descending: true).get();
         if (kDebugMode) {
-          print(docs.size);
+          print(questionsDocs.size);
         }
         int i = 0;
-        for (var doc in docs.docs) {
+        for (var doc in questionsDocs.docs) {
           if (kDebugMode) {
-            print(i++);
-            print(doc.id);
-
-            if (doc.id.startsWith('2NogiaAijcQrNwWO4Tj8')) {
+            if (doc.id.startsWith('NtvI')) {
               print(doc.id);
             }
+            if(i == 29){
+              print(doc.id);
+            }
+            i++;
           }
           SelectQuestion sq = SelectQuestion.fromJson(doc.data());
           sq.id = doc.id;
-          Map<String, dynamic>? answer;
-          if (doc
-              .data()
-              .keys
-              .contains('answers')) {
-            QuerySnapshot<Map<String, dynamic>> answers = await db.collection(
-                'questions').doc(doc.id).collection('answers').get();
-            sq.answer = Answer.fromJson(answers.docs.first.data());
-          } else {
-            sq.answer = Answer();
-          }
+          // var aws = await db.collection(kQuestionTable).doc(doc.id).path('answers').get();
+          // sq.answer = Answer.fromJson(aws.docs.first.data());
           result.add(sq);
+        }
+
+        var json = jsonEncode(result.map((e) => e.toJson()).toList());
+        if (await storageService.saveQuestionsToLocalAsync(jsonEncode(json))) {
+          print('saved');
         }
         print('from online loaded');
       } catch (ex) {
@@ -82,39 +59,26 @@ class QuestionService {
           print(ex);
         }
       }
-    }
-
-    else {
-      String? questionsAsString =
-      await storageService.loadQuestionsFromLocalAsJsonAsync();
+    } else {
+      String? questionsAsString = await storageService
+          .loadQuestionsFromLocalAsJsonAsync();
       if (questionsAsString != null) {
-        //  results =  (json.decode(questionsAsString!) as List).map((i) =>
-        //     SelectQuestion.fromJson(i)).toList();
-        try {
-          //  final List<dynamic> dataList = jsonDecode(questionsAsString!);
-          //print(dataList[0]); // {text: foo, value: 1, status: true}
-          //  print(dataList[1]); // {text: bar, value: 2, status: false}
-          // print('from local loaded');
-        } catch (ex) {
-          print(ex);
-        }
+        List<dynamic> parsedListJson = jsonDecode(questionsAsString);
+        result = List<SelectQuestion>.from(
+            parsedListJson.map<SelectQuestion>((dynamic i) =>
+                SelectQuestion.fromJson(i)));
+        print('from local loaded');
       }
     }
-    return
-
-      result
-
-    ;
+    return result;
   }
 
   Future<bool> updateQuestionAsync(SelectQuestion question) async {
     final db = FirebaseFirestore.instance;
-    final questionDocument = db.collection("questions").doc(question.id);
-    await questionDocument.set(question.toJson());
-    await questionDocument
-        .collection('answers')
-        .doc()
-        .set(question.answer.toJson());
+    final questionDocument = db.collection(kQuestionTable).doc(question.id);
+    questionDocument.set(question.toJson());
+    db.collection(kQuestionTable).doc(question.id).update(
+        {"answers": question.answer.toJson()});
     return Future.value(true);
   }
 }
